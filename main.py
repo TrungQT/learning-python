b31bffe164fa273f5e6dc3eb1e6365104a0f6d83
Trung <tq-trinh@hotmail.com>
2022-03-13 23:47:30 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ def days_to_units(num_of_days):
     return f"{num_of_days} days are {num_of_days * calculation_to_hours} {name_of_unit}"
 
 
-
 def validate_and_execute():
     try:
 
-        user_input_number = int(user_input)
+        user_input_number = int(num_of_days_element)
         if user_input_number > 0:
             converted_value = days_to_units(user_input_number)
             print(converted_value)
@@ -23,11 +22,12 @@ def validate_and_execute():
 
 user_input = ""
 while user_input != "exit":
-    user_input = input("Enter a number of days to be converted into hours\n")
-    for num_of_days_element in user_input.split(","):
+    user_input = input("Enter a number of days to be converted into hours (as a comma seperated list)\n")
+    print(type(user_input.split(", ")))
+    print(user_input.split(","))
+    for num_of_days_element in user_input.split(", "):
         validate_and_execute()
     print(f"Please type 'exit' if you would like to close the program")
-
 
 
 
